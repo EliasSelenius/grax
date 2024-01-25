3e154dadab38100706adf652921b8deb31e38c3e
Elias Sebastian Selenius Haneborg <ESSH@LIVE.NO>
2024-01-25 15:44:00 +0100

    gizmo_sphere() and point size

--- shaders/gizmo_points3D.glsl
+++ shaders/gizmo_points3D.glsl
@@ -12,8 +12,8 @@ IO FragData {
 
 layout (location = 0) in vec3 a_Pos;
 layout (location = 1) in vec4 a_Color;
+layout (location = 2) in float a_Size;
 
-const float pointSize = 0.1;
 
 // from: https://github.com/devoln/synthgen-particles-win/blob/master/src/shader.h
 float calcPointSize(vec4 viewPos, float size) {
@@ -26,7 +26,7 @@ void main() {
     vec4 viewPos = camera.view * vec4(a_Pos, 1.0);
 
     v2f.color = a_Color;
-    gl_PointSize = calcPointSize(viewPos, pointSize);
+    gl_PointSize = calcPointSize(viewPos, a_Size);
     gl_Position = camera.projection * viewPos;
 }
 
