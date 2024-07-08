86de000a354d218e1b0ef9bf0b42cf82f3dae0fb
Elias Sebastian Selenius Haneborg <ESSH@LIVE.NO>
2024-07-09 00:30:57 +0200

    image flood_fill and get_all_regions, for spritesheets

--- shaders/common.glsl
+++ shaders/common.glsl
@@ -8,4 +8,26 @@
 float maxdot(vec3 a, vec3 b) { return max(0.0, dot(a, b)); }
 float sq(float x) { return x*x; }
 
+
+mat3 create_mat3(vec2 pos, float rot, vec2 scale) {
+    float c = cos(rot);
+    float s = sin(rot);
+    return mat3(
+        c * scale.x, s * scale.y, pos.x,
+        -s * scale.x, c * scale.y, pos.y,
+        0, 0, 1
+    );
+}
+
+mat3 create_mat3_inv(vec2 pos, float rot, vec2 scale) {
+    float c = cos(rot) / scale.x; // TODO: the scalings here must certanly be wrong, look at create_mat3()
+    float s = sin(rot) / scale.y;
+    return mat3(
+        c, -s, -dot(pos, vec2(c, -s)),
+        s,  c, -dot(pos, vec2(s, c)),
+        0,  0, 1
+    );
+}
+
+
 #endif
--- shaders/default2d.glsl
+++ shaders/default2d.glsl
@@ -12,41 +12,25 @@ uniform vec3 entity_pos;
 uniform float entity_rot = 0;
 uniform vec2 entity_scale = vec2(1, 1);
 
+uniform vec2 uv_offset = vec2(0.0);
+uniform vec2 uv_scale  = vec2(1.0);
+
 #ifdef VertexShader /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
 #include "../grax/shaders/app.glsl"
-
-mat3 createMatrix(vec2 pos, float rot, vec2 scale) {
-    float c = cos(rot);
-    float s = sin(rot);
-    return mat3(
-        c * scale.x, s * scale.y, pos.x,
-        -s * scale.x, c * scale.y, pos.y,
-        0, 0, 1
-    );
-}
-
-mat3 createMatrixInv(vec2 pos, float rot, vec2 scale) {
-    float c = cos(rot) / scale.x; // TODO: the scalings here must certanly be wrong, look at createMatrix()
-    float s = sin(rot) / scale.y;
-    return mat3(
-        c, -s, -dot(pos, vec2(c, -s)),
-        s,  c, -dot(pos, vec2(s, c)),
-        0,  0, 1
-    );
-}
+#include "../grax/shaders/common.glsl"
 
 layout (location = 0) in vec2 a_Pos;
 layout (location = 1) in vec2 a_Uv;
 layout (location = 2) in vec4 a_Color;
 
 void main() {
-    v2f.uv = a_Uv;
+    v2f.uv = uv_offset + a_Uv * uv_scale;
     v2f.color = a_Color;
 
     vec3 v = vec3(a_Pos, 1);
-    v *= createMatrix(entity_pos.xy, entity_rot, entity_scale);
-    v *= createMatrixInv(cam_pos, cam_rot, vec2(zoom));
+    v *= create_mat3(entity_pos.xy, entity_rot, entity_scale);
+    v *= create_mat3_inv(cam_pos, cam_rot, vec2(zoom));
 
     v.x *= Aspect;
     gl_Position = vec4(v.xy, entity_pos.z, 1.0 + entity_pos.z);
@@ -56,7 +40,7 @@ void main() {
 #ifdef FragmentShader ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
 uniform sampler2D tex;
-uniform vec4 color_factor;
+uniform vec4 color_factor = vec4(1.0);
 
 const vec3 background_color = vec3(0.1);
 
