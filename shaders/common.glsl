cf4668217445da2af2268c2ec631237d84908c43
Elias Sebastian Selenius Haneborg <ESSH@LIVE.NO>
2026-02-06 01:11:55 +0100

    instancing and bindless textures

--- shaders/common.glsl
+++ shaders/common.glsl
@@ -40,5 +40,63 @@ mat3 create_mat3_inv(vec2 pos, float rot, vec2 scale) {
     );
 }
 
+vec4 quat_from_axis_angle(vec3 axis, float angle) {
+    float ha = angle / 2.0;
+    return vec4(axis * sin(ha), cos(ha));
+}
+
+vec4 quat_mul(vec4 l, vec4 r) {
+    float a = l.w;
+    float b = l.x;
+    float c = l.y;
+    float d = l.z;
+    float e = r.w;
+    float f = r.x;
+    float g = r.y;
+    float h = r.z;
+
+    return vec4(
+        b*e + a*f + c*h - d*g,
+        a*g - b*h + c*e + d*f,
+        a*h + b*g - c*f + d*e,
+        a*e - b*f - c*g - d*h
+    );
+}
+
+vec3 rotate_by_quat(vec4 q, vec3 v) {
+    vec4 conj = vec4(-q.xyz, q.w);
+    return quat_mul(quat_mul(q, vec4(v, 0.0)), conj).xyz;
+}
+
+vec3 normal_from_sampler(sampler2D sam, vec2 uv, vec3 surface_normal) {
+    /*
+    // random github. luma
+    return dot(color, vec3(0.299, 0.587, 0.114));
+
+    // learnopengl.com
+    float brightness = dot(color, vec3(0.2126, 0.7152, 0.0722));
+    */
+
+    // vec3 color = texture(sam, uv).rgb;
+    // float b = dot(color, vec3(0.299, 0.587, 0.114));
+    // float b = dot(color, vec3(0.2126, 0.7152, 0.0722));
+    // vec2 grad = vec2(dFdx(b), dFdy(b));
+
+    vec2 size = textureSize(sam, 0);
+    float pix = 1.0 / size.x;
+
+    float b0 = dot(texture(sam, uv+vec2(0.0, 0.0)).rgb, vec3(0.299, 0.587, 0.114));
+    float b1 = dot(texture(sam, uv+vec2(pix, 0.0)).rgb, vec3(0.299, 0.587, 0.114));
+    float b2 = dot(texture(sam, uv+vec2(0.0, pix)).rgb, vec3(0.299, 0.587, 0.114));
+
+    vec2 grad = vec2(b1 - b0, b2 - b0);// / (pix*100.0);
+    vec3 norm = normalize(vec3(-grad.x, 1, -grad.y));
+
+    if (surface_normal.y < 0) norm = -norm;
+
+    vec3 cro = cross(vec3(0,1,0), surface_normal);
+    vec4 q = quat_from_axis_angle(noz(cro), length(cro));
+    return rotate_by_quat(q, norm);
+}
 
 #endif
--- shaders/gpass.glsl
+++ shaders/gpass.glsl
@@ -13,21 +13,28 @@
     flat int instance_id;\
 }\
 
-layout (std140) uniform Instances {
-    mat4 transform[16];
-} instances;
-
-layout (std430) readonly buffer Textures {
-    sampler2D textures[];
+struct InstanceData {
+    mat4 model;
+    vec4 uv_offset_scale;
+    vec4 albedo_color;
+    vec2 metallic_roughness;
+    sampler2D albedo_texture;
 };
 
-uniform sampler2D albedo_texture;
-uniform vec2 u_uv_offset = vec2(0.0);
-uniform vec2 u_uv_scale = vec2(1.0);
-uniform vec3  albedo_color;
-uniform float metallic;
-uniform float roughness;
+// struct Material {
+//     vec4 albedo_color;
+//     vec2 metallic_roughness;
+//     sampler2D albedo_texture;
+// };
 
+layout (std140) readonly buffer Instances {
+    InstanceData instances[];
+};
+
+// std430
+layout (std140) readonly buffer Textures {
+    sampler2D textures[];
+};
 
 
 #ifdef VertexShader /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
@@ -38,14 +45,20 @@ layout (location = 2) in vec2 a_Uv;
 out IO_Data vert_output;
 
 void main() {
-    mat4 model = instances.transform[gl_InstanceID];
+    InstanceData data = instances[gl_InstanceID];
+
+    mat4 model = data.model;
     mat4 view_model = camera.view * model;
     vec4 view_pos = view_model * vec4(a_Pos, 1.0);
+
+    vec2 uv_offset = data.uv_offset_scale.xy;
+    vec2 uv_scale  = data.uv_offset_scale.zw;
+
 
     vert_output.view_pos = view_pos.xyz;
     vert_output.world_normal = mat3(model) * a_Normal;
     vert_output.view_normal = mat3(view_model) * a_Normal;
-    vert_output.uv = u_uv_offset + a_Uv * u_uv_scale;
+    vert_output.uv = uv_offset + a_Uv * uv_scale;
     vert_output.instance_id = gl_InstanceID;
 
     gl_Position = camera.projection * view_pos;
@@ -54,66 +67,6 @@ void main() {
 
 
 #ifdef FragmentShader ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-
-vec4 quat_from_axis_angle(vec3 axis, float angle) {
-    float ha = angle / 2.0;
-    return vec4(axis * sin(ha), cos(ha));
-}
-
-vec4 quat_mul(vec4 l, vec4 r) {
-    float a = l.w;
-    float b = l.x;
-    float c = l.y;
-    float d = l.z;
-    float e = r.w;
-    float f = r.x;
-    float g = r.y;
-    float h = r.z;
-
-    return vec4(
-        b*e + a*f + c*h - d*g,
-        a*g - b*h + c*e + d*f,
-        a*h + b*g - c*f + d*e,
-        a*e - b*f - c*g - d*h
-    );
-}
-
-vec3 rotate_by_quat(vec4 q, vec3 v) {
-    vec4 conj = vec4(-q.xyz, q.w);
-    return quat_mul(quat_mul(q, vec4(v, 0.0)), conj).xyz;
-}
-
-vec3 normal_from_sampler(sampler2D sam, vec2 uv, vec3 surface_normal) {
-    /*
-    // random github. luma
-    return dot(color, vec3(0.299, 0.587, 0.114));
-
-    // learnopengl.com
-    float brightness = dot(color, vec3(0.2126, 0.7152, 0.0722));
-    */
-
-    // vec3 color = texture(sam, uv).rgb;
-    // float b = dot(color, vec3(0.299, 0.587, 0.114));
-    // float b = dot(color, vec3(0.2126, 0.7152, 0.0722));
-    // vec2 grad = vec2(dFdx(b), dFdy(b));
-
-    vec2 size = textureSize(sam, 0);
-    float pix = 1.0 / size.x;
-
-    float b0 = dot(texture(sam, uv+vec2(0.0, 0.0)).rgb, vec3(0.299, 0.587, 0.114));
-    float b1 = dot(texture(sam, uv+vec2(pix, 0.0)).rgb, vec3(0.299, 0.587, 0.114));
-    float b2 = dot(texture(sam, uv+vec2(0.0, pix)).rgb, vec3(0.299, 0.587, 0.114));
-
-    vec2 grad = vec2(b1 - b0, b2 - b0);// / (pix*100.0);
-    vec3 norm = normalize(vec3(-grad.x, 1, -grad.y));
-
-    if (surface_normal.y < 0) norm = -norm;
-
-    vec3 cro = cross(vec3(0,1,0), surface_normal);
-    vec4 q = quat_from_axis_angle(noz(cro), length(cro));
-    return rotate_by_quat(q, norm);
-}
-
 layout (location = 0) out vec4 FragPos_Metallic;
 layout (location = 1) out vec4 FragNormal_Roughness;
 layout (location = 2) out vec3 FragColor;
@@ -121,8 +74,14 @@ layout (location = 2) out vec3 FragColor;
 in IO_Data frag_input;
 
 void main() {
+    InstanceData data = instances[frag_input.instance_id];
+
+    float metallic  = data.metallic_roughness.x;
+    float roughness = data.metallic_roughness.y;
+
+    sampler2D tex = sampler2D(data.albedo_texture);
     // sampler2D tex = textures[frag_input.instance_id];
-    sampler2D tex = albedo_texture;
+    // sampler2D tex = albedo_texture;
 
     vec3 world_normal = normalize(frag_input.world_normal);
     vec3 normal = mat3(camera.view) * normal_from_sampler(tex, frag_input.uv, world_normal);
@@ -132,7 +91,7 @@ void main() {
     vec4 tex_color = texture(tex, frag_input.uv);
     if (tex_color.a == 0.0) discard;
 
-    FragColor = tex_color.rgb * albedo_color;
+    FragColor = tex_color.rgb * data.albedo_color.rgb;
     // FragColor = albedo_color;
     // FragColor = normal;
 
@@ -140,3 +99,4 @@ void main() {
     FragNormal_Roughness = vec4(normalize(normal), roughness);
 }
 #endif
+
